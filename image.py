9f388f0c9253dd26b95980a0c60220866b82321e
Schmidt Léa Renée Olga <lschmidt@SVBBCF20170046.intranet.epfl.ch>
2017-10-31 12:15:51 +0100

    Neuron are connected between them and the Network work (still some problems with my potential)

--- image.py
+++ image.py
@@ -4,6 +4,6 @@ import pylab as pl
 data = np.genfromtxt('spikes.txt')
 
 select = np.array([d for d in data if d[1] < 30])
-datal = select.transpose()
+data1 = select.transpose()
 pl.scatter(0.1*data1[0], data1[1], alpha = 0.8, edgecolors = 'none');
 pl.show();
